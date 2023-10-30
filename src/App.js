37d67d67e5c1215392cf46b682e525ba8cb65e50
B. E <43467866+90VSELH@users.noreply.github.com>
2023-10-30 05:58:27 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState, useEffect, useRef } from "react";
+import exifr from "exifr";
+import "./styles.css";
+
+// const localS = (file) => window.localStorage.setItem("img", file.name);
+export default function App() {
+  const [file, setFile] = useState("");
+  const [imgSrc, setImgSrc] = useState();
+  const [loading, setLoading] = useState(true);
+  // const [gps, setGps] = useState({ latitude: 0, longitude: 0 });
+  // const currentRef = useRef();
+  const handleChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const readFile = async () => {
+    const reader = new FileReader();
+
+    await reader.addEventListener(
+      "load",
+      () => {
+        // console.log(reader.result);
+        setImgSrc(reader.result);
+        setLoading(false);
+      },
+      false
+    );
+    if (file) {
+      await reader.readAsDataURL(file);
+
+      try {
+        let { latitude, longitude } = await exifr.gps(file);
+        console.log(latitude, longitude);
+      } catch (e) {
+        console.error('"No gps Meta Data :) Try another pix"');
+        throw new Error("No gps Meta Data :) Try another pix");
+      }
+    }
+  };
+  useEffect(() => {
+    readFile();
+  });
+  return (
+    <div className="App">
+      <h1>Load an Image</h1>
+      <h2>only React</h2>
+      <input type="file" onChange={handleChange} />
+
+      <div>{!loading && <img id="output" src={imgSrc} />}</div>
+    </div>
+  );
+}
